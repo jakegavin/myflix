2cb65fda530689b53bd6808c054d5ed9e48a88a5
Jake Gavin <jakegavin@gmail.com>
2014-03-24 21:45:46 -0700

    Add queue items

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -3,15 +3,6 @@ class QueueItemsController < ApplicationController
 
   def index
     @queue_items = current_user.queue_items
-    @ratings = []
-    @queue_items.each do |item|
-      reviews = Review.where(user: current_user, video: item.video)
-      if reviews.empty?
-        @ratings.push("")
-      else
-        @ratings.push(reviews[0].rating)
-      end
-    end
+    binding.pry
   end
-
 end
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -4,4 +4,21 @@ class QueueItem < ActiveRecord::Base
 
   validates :video_id, presence: true
   validates :user_id, presence: true
+
+  def video_title
+    video.title
+  end
+
+  def rating
+    reviews = Review.where(user_id: user_id, video_id: video_id).order("created_at DESC")
+    if reviews.empty?
+      return ""
+    else
+      reviews[0].rating
+    end
+  end
+
+  def genres
+    video.categories
+  end
 end
