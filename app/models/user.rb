d4763ba60e5d7af24cf85b2d16011c49d176b4b7
Jake Gavin <jakegavin@gmail.com>
2014-03-31 00:21:08 -0700

    Change normalize method name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :password, length: { minimum: 5 }, on: :create
 
-  def normalize_queue_items
+  def normalize_queue_items_positions
     queue_items.each_with_index do |item, i|
       item.position = i+1
       item.save!
