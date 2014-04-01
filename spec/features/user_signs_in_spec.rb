52665b03186763b28b63fc79d31aae54ad0cbc27
Jake Gavin <jakegavin@gmail.com>
2014-04-01 00:58:50 -0700

    Create user signs in feature spec

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'user signs in' do
+  background do
+    User.create(name: "Test User", email: "testing@gmail.com", password: "testing")
+  end
+  scenario 'with valid email and password' do
+    visit login_path
+    fill_in "Email Address", with: "testing@gmail.com"
+    fill_in "Password", with: "testing"
+    click_button "Sign In"
+    expect(page).to have_text "Welcome, Test User"
+  end
+end
