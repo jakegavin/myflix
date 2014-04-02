a2325e8599140e2cbc17642fb906b7f8b80743ac
Jake Gavin <jakegavin@gmail.com>
2014-04-01 19:05:08 -0700

    Clean up feature specs

--- spec/features/queue_interaction_spec.rb
+++ spec/features/queue_interaction_spec.rb
@@ -4,64 +4,64 @@ feature "Queue interaction" do
 
   given!(:south_park) { Fabricate(:video, title: "South Park", categories: [Fabricate(:category)], small_cover_url: "/tmp/sp.jpg", description: "An ongoing narrative revolving around four boys—Stan Marsh, Kyle Broflovski, Eric Cartman, and Kenny McCormick—and their bizarre adventures in and around the titular Colorado town." ) }
   given!(:futurama) { Fabricate(:video, title: "Futurama", categories: [Fabricate(:category)], description: "A pizza delivery boy awakens in the 31st century after 1,000 years of cryogenic preservation and finds a job at an interplanetary delivery service.", small_cover_url: "/tmp/futurama.jpg") }
-  given!(:archer) { Fabricate(:video, title: "Archer", categories: [Fabricate(:category)], description: "Sophisticated spy Archer may have the coolest gadgets, but he still has issues when it comes to dealing with his boss -- who also is his mother.", small_cover_url: "/tmp/archer.jpg")}
-
-  background do
-    Fabricate(:user, name: "Jake Gavin", email: "jakegavin@gmail.com", password: "jakegavin")
-  end
+  given!(:archer) { Fabricate(:video, title: "Archer", categories: [Fabricate(:category)], description: "Sophisticated spy Archer may have the coolest gadgets, but he still has issues when it comes to dealing with his boss -- who also is his mother.", small_cover_url: "/tmp/archer.jpg") }
+  given!(:jake) { Fabricate(:user, name: "Jake Gavin", email: "jakegavin@gmail.com", password: "jakegavin") }
 
   scenario "User adds and reorders queue items" do
-    visit root_path
-    click_link "sign in"
-    
-    expect(page).to have_text "Sign in"
-    
-    fill_in "Email Address", with: "jakegavin@gmail.com"
-    fill_in "Password", with: "jakegavin"
-    click_button("Sign in")
-
+    sign_in(jake)
     expect(page).to have_text "Welcome, Jake Gavin"
 
-    find(:xpath, "//a/img[@src='#{south_park.small_cover_url}']/ancestor::a[1]").click
-
-    expect(page).to have_text south_park.description
-
-    click_link "+ My Queue"
-    
+    add_video_to_queue(south_park)
+    expect(page).to have_content south_park.title
     expect(page).to have_text "List Order"
     expect(page).to have_text "Video Title"
 
     click_link "South Park"
 
     expect(page).to have_text south_park.description
-    expect(page).to_not have_text "+ My Queue"
+    expect_link_to_not_be_seen("+ My Queue")
 
-    click_link "Videos"
-    find(:xpath, "//a/img[@src='#{futurama.small_cover_url}']/ancestor::a[1]").click
-    click_link "+ My Queue"
+    add_video_to_queue(futurama)
+    add_video_to_queue(archer)
 
-    click_link "Videos"
-    find(:xpath, "//a/img[@src='#{archer.small_cover_url}']/ancestor::a[1]").click
-    click_link "+ My Queue"
+    expect_queue_to_have_videos([south_park, futurama, archer])
+    expect(find('#item_0_title')).to have_text south_park.title
+    expect(find('#item_1_title')).to have_text futurama.title
+    expect(find('#item_2_title')).to have_text archer.title
 
-    expect(page).to have_text south_park.title
-    expect(page).to have_text futurama.title
-    expect(page).to have_text archer.title
-
-    find('#item_0_title').should have_text south_park.title
-    find('#item_1_title').should have_text futurama.title
-    find('#item_2_title').should have_text archer.title
-
-    fill_in 'item_0_position', with: '4'
-    fill_in 'item_1_position', with: '3'
-    fill_in 'item_2_position', with: '5'
+    fill_in_video_position(south_park, 4)
+    fill_in_video_position(futurama, 3)
+    fill_in_video_position(archer, 5)
     click_button ('Update Instant Queue')
 
-    expect(find_field('item_0_position').value).to eq "1"
+    expect(find("input[data-pos-video-id='#{futurama.id}']").value).to eq "1"
     expect(find('#item_0_title')).to have_text futurama.title
-    expect(find_field('item_1_position').value).to eq "2"
+    expect(find("input[data-pos-video-id='#{south_park.id}']").value).to eq "2"
     expect(find('#item_1_title')).to have_text south_park.title
-    expect(find_field('item_2_position').value).to eq "3"
+    expect(find("input[data-pos-video-id='#{archer.id}']").value).to eq "3"
     expect(find('#item_2_title')).to have_text archer.title
   end
+
+  def expect_link_to_not_be_seen(link_text)
+    expect(page).to_not have_content link_text
+  end
+
+  def expect_queue_to_have_videos(videos)
+    visit queue_path
+    videos.each do |video|
+      expect(page).to have_text video.title
+    end
+  end
+
+  def fill_in_video_position(video, position) do
+    find("input[data-pos-video-id='#{video.id}']").set(position)
+  end
+
+  def add_video_to_queue(video)
+    visit home_path
+    find(:xpath, "//a/img[@src='#{video.small_cover_url}']/ancestor::a[1]").click
+    click_link "+ My Queue"
+  end
+
+
 end
--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,14 +1,10 @@
 require 'spec_helper'
 
 feature 'user signs in' do
-  background do
-    User.create(name: "Test User", email: "testing@gmail.com", password: "testing")
-  end
+  given!(:user) { User.create(name: "Test User", email: "testing@gmail.com", password: "testing") }
+
   scenario 'with valid email and password' do
-    visit login_path
-    fill_in "Email Address", with: "testing@gmail.com"
-    fill_in "Password", with: "testing"
-    click_button "Sign in"
+    sign_in(user)
     expect(page).to have_text "Welcome, Test User"
   end
 end
