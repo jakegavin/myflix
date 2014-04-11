c8b40b603287e7c91a6530ae7193ed3243c3dcb3
Jake Gavin <jakegavin@gmail.com>
2014-04-11 01:25:19 -0700

    Update routes for new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,13 @@ Myflix::Application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
-  get 'forgot_password', to: 'users#forgot_password'
-  post 'forgot_password', to: 'users#generate_token'
-  get 'password_reset_confirmation', to: 'static_pages#password_reset_confirmation'
-  get 'reset_password/(:password_reset_token)', to: 'users#reset_password', as: :reset_password
-  post 'reset_password', to: 'users#reset_password'
-  get 'invalid_token', to: 'static_pages#invalid_token'
 
+  resources :forgot_passwords, only: [:create]
+  get 'forgot_password', to: 'forgot_passwords#new'
+  get 'password_reset_confirmation', to: 'forgot_passwords#password_reset_confirmation'
+  resources :reset_passwords, only: [:show, :create]
+  get 'invalid_token', to: 'reset_passwords#invalid_token'
+  
   resources :queue_items, only: [:destroy]
   post 'modify_queue', to: 'queue_items#modify'
   get 'queue', to: 'queue_items#index'
