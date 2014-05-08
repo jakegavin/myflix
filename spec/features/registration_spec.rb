4ad03ff1679f57761d39078553bf7bff1cc85217
Jake Gavin <jakegavin@gmail.com>
2014-05-08 09:03:54 -0700

    Add registration feature spec

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'User registration', { js: true, vcr: true } do    
+  scenario 'user has valid inputs', driver: :selenium do
+    visit register_path
+    fill_in 'Full Name', with: 'Bob'
+    fill_in 'Email Address', with: 'bob@gmail.com'
+    fill_in 'Password', with: 'password'
+
+    fill_in 'Credit Card Number', with: '4242424242424242'
+    fill_in 'Security Code', with: '123'
+    select '1 - January', from: 'date_month'
+    select '2017', from: 'date_year'
+
+    click_button 'Sign Up'
+  end
+  # scenario 'user has invalid user attributes and valid credit card'
+  # scenario 'user has valid user attributes and invalid credit card'
+  # scenario 'user has valid attributes but credit card is declined'
+end
+
