625b7501cf479eb063584492c92562531a8dae5e
Jake Gavin <jakegavin@gmail.com>
2014-05-08 09:09:55 -0700

    Add default cassette option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |c|
+  c.default_cassette_options = { :record => :new_episodes, :erb => true }
   c.cassette_library_dir = 'spec/support/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
